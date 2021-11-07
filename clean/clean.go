4d45600f8c9839a65274a69e40d113f501ec390a
Guillaume MORIN <guillaume.morin@epitech.eu>
2021-11-07 12:49:22 +0400

    mv generated file to api folder

--- clean/clean.go
+++ clean/clean.go
@@ -7,7 +7,7 @@ import (
 	"os"
 )
 
-func CheckNameFile(api []fs.FileInfo, fileName string) bool {
+func checkNameFile(api []fs.FileInfo, fileName string) bool {
 	for _, v := range api {
 		if v.Name() == fileName {
 			return true
@@ -28,11 +28,25 @@ func main() {
 	}
 
 	for _, f := range currentFolder {
-		if CheckNameFile(apiFolder, f.Name()) {
+		if checkNameFile(apiFolder, f.Name()) {
 			e := os.Remove(f.Name())
 			if e != nil {
 				log.Fatal(e)
 			}
 		}
+	}
+
+	for _, v := range currentFolder {
+		if v.Name() == "http.go" || v.Name() == "main.go" || len(v.Name()) < 3 {
+			continue
+		} else if len(v.Name()) > 3 && v.Name()[len(v.Name())-3:len(v.Name())] == ".go" {
+			if !checkNameFile(apiFolder, v.Name()) {
+				err := os.Rename(v.Name(), "./api/"+v.Name())
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		}
+
 	}
 }
