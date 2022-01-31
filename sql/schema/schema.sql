a59ec95e9a936d69c6a892cd94cc4639687abe86
Guillaume MORIN <guillaume.morin@epitech.eu>
2022-01-31 19:27:21 +0400

    strong table sql

--- sql/schema/schema.sql
+++ sql/schema/schema.sql
@@ -53,6 +53,23 @@ CREATE TABLE "users" (
   "firebase_provider" text DEFAULT NULL
 );
 
+   
+-- CREATE TABLE "test" (
+--   "id" uuid PRIMARY KEY DEFAULT (gen_random_uuid()),
+--   "created_at" timestamp NOT NULL DEFAULT (now()),
+--   "updated_at" timestamp NOT NULL DEFAULT (now()),
+--   "deleted_at" timestamp CONSTRAINT deletedchk CHECK (deleted_at > created_at),
+--   "email" text NOT NULL CONSTRAINT emailchk CHECK (email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'),
+--   "password" text NOT NULL CONSTRAINT passwordchk CHECK (char_length(password) >= 9),
+--   "firstname" text NOT NULL CONSTRAINT firstnamechk CHECK (char_length(firstname) >= 3 AND char_length(firstname) <= 25),
+--   "lastname" text NOT NULL CONSTRAINT lastnamechk CHECK (char_length(lastname) >= 3 AND char_length(lastname) <= 25),
+--   "password_confirm_code" text DEFAULT NULL CONSTRAINT code_passwordchk CHECK (char_length(password_confirm_code) = 5),
+--   "role" role NOT NULL DEFAULT 'user',
+--   "firebase_uid" text DEFAULT NULL UNIQUE,
+--   "firebase_id_token" text DEFAULT NULL UNIQUE,
+--   "firebase_provider" text DEFAULT NULL
+-- );
+
 CREATE TABLE "refresh_token" (
   "id" uuid PRIMARY KEY DEFAULT (gen_random_uuid()),
   "created_at" timestamptz NOT NULL DEFAULT (NOW()),
